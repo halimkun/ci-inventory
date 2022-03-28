e02f0550f0c6c0e9201159866cda2ae970f33ae9
halimkun <ffaisalhalim@gmail.com>
2022-03-28 08:52:47 +0700

    little edit

--- public/styles/js/select2.js
+++ public/styles/js/select2.js
@@ -1,8 +1,8 @@
 $(function() {
   'use strict'
 
-  if ($(".js-example-basic-single").length) {
-    $(".js-example-basic-single").select2();
+  if ($(".select2").length) {
+    $(".select2").select2();
   }
   if ($(".js-example-basic-multiple").length) {
     $(".js-example-basic-multiple").select2();
